f906b076ea4f5bde930f114a0584bc28a252d9c0
TeamWizard <131433417+BOTZHUBX@users.noreply.github.com>
2023-04-21 13:27:32 +0530

    Update password.py

--- plugins/ExraMods/password.py
+++ plugins/ExraMods/password.py
@@ -15,5 +15,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Mᴋɴ Bᴏᴛᴢ™️', url='https://t.me/mkn_bots_updates')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Team Netflix™️', url='https://t.me/team_netflix')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
